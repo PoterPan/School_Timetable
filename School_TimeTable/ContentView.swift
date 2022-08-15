1405d38d2f9575b11178b8eb03c3932941dbf6ed
PoterPan <poterpan5466@gmail.com>
2022-08-15 15:26:30 +0800

    Initial Commit

---
+++ School_TimeTable/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  School_TimeTable
+//
+//  Created by Poter Pan on 2022/8/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let titles = ["週一", "週二", "週三", "週四", "週五"]
+    
+    let cols = Array(repeating: GridItem(), count: 5)
+    
+    @EnvironmentObject var courseVM: CourseViewModel
+    @State private var isPresentingCourse: CourseModel? = nil
+
+
+    
+    var body: some View {
+        ZStack {
+            Color.mint
+                .ignoresSafeArea()
+            VStack {
+                Text("我的課表")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                Text(courseVM.courseData[0].name)
+//                ForEach(courseVM.courseData) { course in
+//                    Text(course.id)
+//                }
+                
+                HStack {
+                    ForEach(titles, id: \.self) { title in
+                        Text(title)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                        Divider()
+                    }
+                }
+                .frame(height: 30)
+                .padding()
+                
+                ScrollView{
+                    LazyVGrid(columns: cols, spacing: 20) {
+                        ForEach(courseVM.courseData) { course in
+                            Button {
+//                                courseVM.testCase(course: course)
+//                                courseVM.editCourse(course: course, newName: "123", newPlace: "123")
+                                print("selected course: \(course.id)")
+                                isPresentingCourse = course
+                            } label: {
+                                CourseView(course: course)
+                            }
+                            
+
+
+                        }
+                        .sheet(item: $isPresentingCourse) { course in
+                            EditingSheetView(course: course)
+                        }
+                    }
+                    .padding()
+                    
+                }
+                
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(CourseViewModel())
+    }
+}
+
+
---
+++ School_TimeTable/CourseViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  CourseViewModel.swift
+//  School_TimeTable
+//
+//  Created by Poter Pan on 2022/8/13.
+//
+
+import Foundation
+import SwiftUI
+
+class CourseViewModel: ObservableObject {
+    @Published var courseData: [CourseModel] = []
+//    {
+        // didSet 屬性觀察器（Property observers)
+//        didSet {
+//            saveData()
+//        }
+//    }
+    
+//    let dataKey: String = "course_list"
+    
+    init() {
+        for _ in (1...40) {
+            let newCourse = CourseModel(name: "未設定", place: "未輸入")
+            courseData.append(newCourse)
+        }
+        
+        
+    }
+    
+    func editCourse(course: CourseModel, newName: String, newPlace: String) {
+        print("target course: \(course.id)")
+        if let index = courseData.firstIndex(where: { $0.id == course.id } ) {
+            courseData[index] = CourseModel(name: newName, place: newPlace)
+            print("Index: \(index)")
+            print("Saved")
+            print("New id: \(courseData[index].id)")
+        }
+        else {
+            print("Data Error")
+            print(course)
+            print(courseData)
+            }
+        
+
+    }
+    
+    func testCase(course: CourseModel) {
+        if let index = courseData.firstIndex(where: { $0.id == course.id } ) {
+            courseData[index] = CourseModel(name: "已編輯", place: "已編輯")
+        }
+    }
+    
+//    func saveData() {
+//        if let encodedData = try? JSONEncoder().encode(courseData) {
+//            UserDefaults.standard.set(encodedData, forKey: dataKey)
+//        }
+//    }
+    
+    
+}
